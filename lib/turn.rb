1fd3e773d580b8b2c49460f83a679084e67ad9ed
Andrew Eames <ajerxs@gmail.com>
2020-10-25 21:32:37 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -15,6 +15,12 @@ def move(array, index, value = "X")
   array[index] = value
 end
 
+def valid_move?(board, index)
+  if index.between?(0,8) && !position_taken?(board, index)
+    return true
+  end
+end
+
 def position_taken?(board, index)
   if board[index] == " " || board[index] == "" || board[index] == nil
     return false
